d41e66e120636ee0920f2b76eeea53eec127b7a3
Jon Grenning <jon.g@shopify.com>
2015-04-22 14:36:48 -0400

    setup git and brew initial setup

--- install.sh
+++ install.sh
@@ -1,40 +1,113 @@
 #!/usr/bin/env sh
 
-# import config file
-source config.cfg
+##############################################################
+################## Configuration #############################
+##############################################################
+
+# Variables
+
+bootstrap_dir=$HOME/.bootstrapify
+config_dir=$bootstrap_dir/lib
+bootstrapify=Bootstrapify
+git_master=git@github.com:jtgrenz/bootstrapify.git
+
+# important commands
+install_homebrew=ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+##############################################################
+################## Begin Install Script ######################
+##############################################################
+
+
+# clear terminal
+clear
+
+# print welcome
+cat << "EOF"
+
+
+                          Welcome to
+##############################################################
+______             _       _                   _  __
+| ___ \           | |     | |                 (_)/ _|
+| |_/ / ___   ___ | |_ ___| |_ _ __ __ _ _ __  _| |_ _   _
+| ___ \/ _ \ / _ \| __/ __| __| '__/ _` | '_ \| |  _| | | |
+| |_/ / (_) | (_) | |_\__ \ |_| | | (_| | |_) | | | | |_| |
+\____/ \___/ \___/ \__|___/\__|_|  \__,_| .__/|_|_|  \__, |
+                                        | |           __/ |
+                                        |_|          |___/
+
+##############################################################
+
+
+EOF
+
+
+
+echo "#### Checking for Xcode commandline tools..."
+echo
 
 # check for xcode
-if [ $(xcode-select -p) != "/Applications/Xcode.app/Contents/Developer" ]; then
+if [[ $(xcode-select -p) != "/Applications/Xcode.app/Contents/Developer" ]]; then
 
-  echo ' You must install xcode commandline tools first. '
-  echo ' Click agree on the prompt to install and run this script again'
+  echo ' !! You must install xcode commandline tools first. '
+  echo ' !! Click agree on the prompt to install and run this script again'
   xcode-select --install
   exit 1
 else
-  echo ' Xcode comandline tools found. Proceeding with bootstrap'
-  exit 1
+  echo '#### Xcode comandline tools found. Proceeding with bootstrap...'
 fi
 
-# # Check for Homebrew
-# if test ! $(which brew); then
-#   echo "Installing homebrew..."
-#   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# fi
 
-# # Update homebrew
-# brew update && brew upgrade brew-cask
 
-# # Install GNU core utilities (those that come with OS X are outdated)
-# brew install coreutils
+echo "#### Checking for existing $bootstrapify install"
+if [[ ! -d $bootstrap_dir ]]; then
+  echo "#### $bootstrapify not found"
+  echo "#### Downloading $bootstrapify..."
+  echo
+  git clone $git_master $bootstrap_dir
+  echo
+else
+  echo "#### $bootstrapify found"
+  echo
+fi
 
-# # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-# brew install findutils
+echo "changing to bootstrap directory..."
+cd $bootstrap_dir
+pwd
 
-# # Install Bash 4
-# brew install bash
-# brew install zsh
+# import config file
+source $config_dir/config.cfg
 
-# # Install more recent versions of some OS X tools
-# brew tap homebrew/dupes
-# brew install homebrew/dupes/grep
+# Check for Homebrew
+if test ! $(which brew); then
+  echo "#### Installing homebrew..."
+  $install_homebrew
+fi
+
+if test ! $(which brew-cask); then
+  echo "#### Installinh homebrew-cask"
+  brew install caskroom/cask/brew-cask
+fi
+
+echo "#### Updating homebrew and homebrew-cask"
+brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
+
+# Install Binaries
+for bin in $Binaries
+do
+  tmp=`brew list | grep $bin`
+  if [[ ! $tmp ]]; then
+    echo ''
+    echo '##### Installing Formula '$bin'...'
+    brew install $bin
+  fi
+done
+
+
+
+
+
+
 
