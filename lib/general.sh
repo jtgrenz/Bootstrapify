b2b7cf56272b7d6c4e3eca4fafeab41163ef27ed
Hiroshi <jtgrenz@users.noreply.github.com>
2015-08-12 16:26:36 -0400

    updated how rbenv is added to profiles

--- lib/general.sh
+++ lib/general.sh
@@ -27,15 +27,21 @@ echo "$FORMAT #### Installing Oh-My-Zsh"
 
 echo "$FORMAT #### Updating Bash and zsh settings"
 # sets up ~/.bin folder for sublime symlink. 
+echo "###### Bootstrapify Additions added $(date)  ###### "
+echo ""
 echo "export PATH=$HOME/.bin:$PATH" >> $bashrc
 echo "export PATH=$HOME/.bin:$PATH" >> $zshrc
 # Sets up path to accept rbenv properly.
 echo "export PATH='~/.rbenv/shims:~/.rbenv/bin:$PATH'" >> $bashrc
 echo "export PATH='~/.rbenv/shims:~/.rbenv/bin:$PATH'" >> $zshrc
+echo "eval "$(rbenv init -)"" >> $zshrc
+echo "eval "$(rbenv init -)"" >> $bashrc
 # Sets sublime text to be the default editor instead of nano or vim
 echo "export EDITOR='subl -w'" >> $bashrc
 echo "export EDITOR='subl -w'" >> $zshrc
-
+echo ""
+echo "###### End of Bootstrapify Additions added $(date)  ###### "
+echo ""
 
 
 
